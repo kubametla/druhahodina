f9e9263b933a3dc7980092412c686485ad14e26e
Jakub Metlička <metlickakuba@gmail.com>
2025-02-11 15:23:01 +0100

    druha hodina

---
+++ Výpis.py
@@ -0,0 +1,17 @@
+jmeno = "Jakub"
+
+prijmeni = "Metlicka"
+
+vek = "16"
+
+vyska = "1.75"
+
+pohlavi = 0
+
+print(jmeno, prijmeni)
+
+print("Je mu", vek, "let")
+
+print("měří", vyska, "metrů")
+
+print(pohlavi)
