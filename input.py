7a63e44f06af3543e37eefa84815bcefa4f68d03
Jakub Metlička <metlickakuba@gmail.com>
2025-02-18 15:04:13 +0100

    Add files via upload

---
+++ input.py
@@ -0,0 +1,40 @@
+sklenice_1 = "mliko"
+
+sklenice_2 = "pivo" 
+
+print("Sklenice1 = mléko")
+print("Sklenice2 = pivo")
+print("Přidáme sklenici")
+
+sklenice_3 = ""
+
+print("sklenice3 = ", sklenice_3)
+print("Právě jsem přelil sklenici1 do sklenice3")
+
+sklenice_1 = ""
+sklenice_2 = "pivo"
+sklenice_3 = "mlíko"
+
+print("sklenice_3 = ",sklenice_3)
+print("sklenice_2 = ",sklenice_2)
+print("sklenice_1 = ",sklenice_1)
+
+print("Právě jsem přelil sklenici2 do sklenice1")
+
+sklenice_1 = "pivo"
+sklenice_2 = ""
+sklenice_3 = "mlíko"
+
+print("sklenice_3 = ",sklenice_3)
+print ("sklenice_2 = ",sklenice_2)
+print("sklenice_1 = ",sklenice_1)
+
+print("Právě jsem přelil sklenici3 do sklenice2")
+
+sklenice_1 = "pivo"
+sklenice_2 = "mlíko"
+sklenice_3 = ""
+
+print("sklenice_3 = ",sklenice_3)
+print("sklenice_2 = ", sklenice_2)
+print("sklenice_1 = ",sklenice_1)
